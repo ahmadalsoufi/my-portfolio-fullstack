084e1b7b4339805eed3d8d4d1b333afc890890ad
ahmadalsoufi <ahmad.s.alsoufi@gmail.com>
2026-09-02 10:08:01 +0300

    add hero section and display featured projects, about, & latest posts at homepage
    - add a layout for home
    - add hero section and display it in home
    - add partial projects, posts, & about sections
    - delete the pagination logic content on blogs' dynamic page that was added mistakingly

--- src/app/(content)/blogs/[slug]/page.tsx
+++ src/app/(content)/blogs/[slug]/page.tsx
@@ -1,27 +1,7 @@
-import { SetStateAction } from "react";
-
-const BlogsDetailsPage = ({
-  totalPages,
-  curPage,
-  setCurPage,
-}: {
-  totalPages: number;
-  curPage: number;
-  setCurPage: React.Dispatch<SetStateAction<number>>;
-}) => {
+const BlogsDetailsPage = () => {
   return (
     <>
-      <div>
-        {Array.from({ length: totalPages }, (_, idx) => (
-          <button
-            key={idx + 1}
-            className="flex"
-            onClick={() => setCurPage(idx + 1)}
-          >
-            {idx + 1}
-          </button>
-        ))}
-      </div>
+      <div></div>
     </>
   );
 };
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,59 @@
 import Hero from "../components/Hero";
+import { getBlogs } from "../(content)/blogs/BlogsProvider";
+import { getProjects } from "../(content)/projects/ProjectsProvider";
+import ProjectCard from "../(content)/projects/ProjectCard";
+import AboutPreview from "../about/AboutPreview";
+import BlogCard from "../(content)/blogs/BlogCard";
+
+import { GiStarsStack } from "react-icons/gi";
+import { IoDocumentText } from "react-icons/io5";
 
 export default async function HomePage() {
+  const recentBlogs = (await getBlogs())
+    ?.slice()
+    .sort((a, b) => {
+      const aDate = new Date(a.event_date).getTime();
+      const bDate = new Date(b.event_date).getTime();
+
+      return aDate - bDate;
+    })
+    .slice(0, 2);
+  const featuredProjects = (await getProjects())?.filter(
+    (project) => project.featured,
+  );
+
   return (
     <>
       <div className="mb-15">
         <Hero />
+      </div>
+
+      <div className="m-5">
+        <section className="mx-auto flex max-w-5xl flex-col justify-center space-y-15">
+          <h1 className="mt-4 mb-5 flex items-center gap-x-2 text-start font-bold capitalize">
+            <GiStarsStack /> Featured projects
+          </h1>
+
+          <div className="mb-5 grid grid-cols-1 gap-3 gap-y-4 text-start sm:grid-cols-2">
+            {featuredProjects?.map((project) => (
+              <ProjectCard key={project.slug} project={project} />
+            ))}
+          </div>
+
+          <div>
+            <AboutPreview detailed={false} />
+          </div>
+
+          <h1 className="mb-5 flex items-center gap-x-2 text-start font-bold capitalize">
+            <IoDocumentText /> Latest posts
+          </h1>
+
+          <div className="mb-5 grid grid-cols-1 gap-3 text-start sm:grid-cols-2">
+            {recentBlogs?.map((blog) => (
+              <BlogCard key={blog.slug} blog={blog} />
+            ))}
+          </div>
+        </section>
       </div>
     </>
   );
